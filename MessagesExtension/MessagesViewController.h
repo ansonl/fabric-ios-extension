734122bcfa41a2a8e155ed66b0f8c7c14c77ccf3
Anson Liu <ansonl@users.noreply.github.com>
2017-04-01 14:17:05 -0400

    Initial Commit

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by Anson Liu on 4/1/17.
+//  Copyright © 2017 Anson Liu. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
